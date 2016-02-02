e7610b4fdaeb3de9a61ea96de6a63c2ef22b5a98
creativcoder <rsconceptx@gmail.com>
2016-02-02 22:42:04 +0530

    updated expr parser

--- kiss-lang/arith_parser.py
+++ kiss-lang/arith_parser.py
@@ -1,6 +1,17 @@
 # Token types
 
-INTEGER, PLUS, EOF = 'INTEGER','PLUS','EOF'
+INTEGER, PLUS, WHITESPACE, EOF = 'INTEGER','PLUS','WHITESPACE','EOF'
+
+class Token:
+    INTEGER = 'INTEGER'
+    PLUS = '+'
+    MINUS = '-'
+    DIV = '/'
+    MULT = '*'
+    MOD = '%'
+    WHITESPACE = ' '
+    EOF = 'EOF'
+
 
 class Token(object):
     def __init__(self,type,value):
@@ -33,4 +44,60 @@ class Interpreter(object):
         text = self.text
 
         if self.pos > len(text) -1:
-            return Token(EOF,None);
+            return Token(EOF,None)
+
+        current_char = text[self.pos]
+        if current_char.isdigit():
+            token = Token(INTEGER,int(current_char))
+            self.pos += 1
+            return token
+
+        if current_char == '+':
+            token = Token(PLUS,current_char)
+            self.pos += 1
+            return token
+
+        self.error()
+
+    def eat(self,token_type):
+        # compare the current token type with the passed token
+        # type and if they match then eat the current token and
+        # assign the next token to the self.current_token,
+        # otherwise raise the exception
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def eval(self):
+        self.current_token = self.get_next_token()
+
+        # we expect the current token to be an integer
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        self.eat(PLUS)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        result = left.value + right.value
+        return result
+
+
+def main():
+    while True:
+        try:
+            text = raw_input('kiss>')
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text.replace(" ",""))
+        result = interpreter.eval()
+        print(result)
+
+if __name__=='__main__':
+    main()
